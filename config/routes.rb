4da6ea23b448b345460c8ef12f2999c84bca8b3c
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-22 19:46:47 +0800

    added routes of houses model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get '/houses', to: 'houses#index', as: :houses
+  get '/houses/:id', to: 'houses#show', as: :house
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
